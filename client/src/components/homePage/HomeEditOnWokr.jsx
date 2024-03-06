414a64cb4d30ff8f09229ca9326d6a3f62300587
muhammadbilal10 <108835912+muhammadbilal10@users.noreply.github.com>
2024-03-06 14:07:05 +0000

    Update Main page

--- client/src/components/homePage/HomeEditOnWokr.jsx
+++ client/src/components/homePage/HomeEditOnWokr.jsx
@@ -25,13 +25,13 @@ const EditonWork = () => {
       </Typography>
 
       <ReactPlayer
-        url="https://www.youtube.com/watch?v=T8LdferdIPA"
+        url="https://www.youtube.com/watch?v=jSNj9Egs6A8"
         height="405px"
         width="720px"
         controls={true}
       />
 
-      <Button
+<Typography
         variant="contained"
         sx={{
           m: "20px 0px",
@@ -43,9 +43,7 @@ const EditonWork = () => {
           alignSelf: "center",
           textTransform: "capitalize",
         }}
-      >
-        More on how EditON works
-      </Button>
+      ></Typography>
     </Box>
   );
 };
